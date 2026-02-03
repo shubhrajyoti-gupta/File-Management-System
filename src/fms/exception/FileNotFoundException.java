dc9d1bf23be0b1668843876babc4d8188a7c4917
Shubhrajyoti Gupta <153622362+shubhrajyoti-gupta@users.noreply.github.com>
2026-02-03 19:54:12 +0530

    Add FileNotFoundException class

---
+++ src/fms/exception/FileNotFoundException.java
@@ -0,0 +1,6 @@
+package fms.exception;
+
+public class FileNotFoundException extends Exception {
+    private static final long serialVersionUID = 1L;
+    public FileNotFoundException(String msg) { super(msg); }
+}
